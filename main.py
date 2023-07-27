e8851d222dd5f34e2f58c3e8f7737e2d9f62c691
CsAjayS <ajaysince2003@gmail.com>
2023-07-27 19:55:25 +0530

    up

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-print("hello world")
-print("hello world")
+for i in range (3,0,-1):
+    for j in range (i):
+        print("2",end="")
+        
+    print()
+    for i in range (3,0,-1):
+      for j in range (i):
+        print("1",end="")
+        
+    print()
+    
